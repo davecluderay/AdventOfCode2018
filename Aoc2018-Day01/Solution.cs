b0840bf5b064e48fac0cca63019cb0c3e09d31a5
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-01 21:59:15 +0000

    Add solution for day 1.

---
+++ Aoc2018-Day01/Solution.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2018_Day01
+{
+    internal class Solution
+    {
+        public string Title => "Day 1: Chronal Calibration";
+
+        public object PartOne()
+        {
+            return ReadAdjustments().Sum();
+        }
+        
+        public object PartTwo()
+        {
+            var sums = new HashSet<long>();
+            var currentFrequency = 0L;
+            
+            var adjustments = ReadAdjustments();
+            for (var adjustmentIndex = 0; ; adjustmentIndex = (adjustmentIndex + 1) % adjustments.Count)
+            {
+                var adjustment = adjustments[adjustmentIndex];
+
+                currentFrequency += adjustment;
+                if (sums.Contains(currentFrequency))
+                {
+                    return currentFrequency;
+                }
+
+                sums.Add(currentFrequency);
+            }
+        }
+
+        private IList<int> ReadAdjustments() => InputFile.ReadAllLines()
+                                                         .Select(int.Parse)
+                                                         .ToList();
+    }
+}
