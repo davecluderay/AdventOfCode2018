92c9a9c813e8c14ed00e2adc09d26d30a6feedbc
Dave Cluderay <dave.cluderay@bybox.com>
2020-03-21 20:45:29 +0000

    Add solution for day 3.

---
+++ Aoc2018-Day03/Solution.cs
@@ -0,0 +1,19 @@
+namespace Aoc2018_Day03
+{
+    internal class Solution
+    {
+        public string Title => "Day 3: No Matter How You Slice It";
+
+        public object? PartOne()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+        
+        public object? PartTwo()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+    }
+}
