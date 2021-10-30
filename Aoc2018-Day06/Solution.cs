f172fb4e926e4aaad1da8f06ca003a6eea91fb30
Dave Cluderay <daveclud@hotmail.com>
2021-10-30 10:15:43 +0100

    Add empty project for day 6.

---
+++ Aoc2018-Day06/Solution.cs
@@ -0,0 +1,19 @@
+namespace Aoc2018_Day06
+{
+    internal class Solution
+    {
+        public string Title => "Day 6: Chronal Coordinates";
+
+        public object? PartOne()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+
+        public object? PartTwo()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+    }
+}
